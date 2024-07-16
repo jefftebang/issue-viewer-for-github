a8d7a36fb7c3022441c0d4b185796446a6c479ec
Jeffersson Tebang <jefftebang0118@gmail.com>
2024-07-16 22:57:42 +0800

    changed bg color of nav and added shadow

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex justify-center bg-slate-400 py-2">
+<nav class="flex justify-center bg-white py-2 drop-shadow-md shadow-lg">
   <div class="container flex justify-between">
     <div>
       <a href="/" class="text-xl text-sky-600">
