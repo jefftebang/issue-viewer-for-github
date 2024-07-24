8dd7d6bec61d758b1ec9c187ac26e57ea85e8ca3
Jeffersson Tebang <jefftebang0118@gmail.com>
2024-07-25 01:48:33 +0800

    added ternary condition to issue body

--- resources/views/pages/issue-details-page.blade.php
+++ resources/views/pages/issue-details-page.blade.php
@@ -45,7 +45,7 @@ $(document).ready(function(){
                 `+response.title+` #`+response.number+`
               </h1>
               <div class="my-5 py-5 px-3 bg-slate-100 rounded-md">
-                `+response.body+`
+                `+(response.body !== null ? response.body : `<em>"No Description"</em>`)+`
               </div>
               <div>
                 `;
@@ -85,4 +85,20 @@ $(document).ready(function(){
     }
   }
 });
+
+// function fizzBuzz() {
+//   for (i = 1; i < 101; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log(i + " - FizzBuzz")
+//     } else if (i % 3 === 0) {
+//       console.log(i + " - Fizz")
+//     } else if (i % 5 === 0) {
+//       console.log(i + " - Buzz")
+//     } else  {
+//       console.log(i)
+//     }
+//   }
+// }
+
+// fizzBuzz();
 </script>
